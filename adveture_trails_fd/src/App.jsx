0c37f460917b7cff79447dea43bbe70b39b7aa9c
juniorpinhodev <juniorpinhodev@gmail.com>
2024-04-05 19:08:24 -0300

    Exibindo os cards

--- adveture_trails_fd/src/App.jsx
+++ adveture_trails_fd/src/App.jsx
@@ -1,13 +1,22 @@
-import './App.css'
-import CardTrilha from './Components/CardTrilha'
+import React from 'react';
+import './App.css';
+import CardTrilha from './Components/CardTrilha';
 
 function App() {
- 
   return (
-    <div>
-      <CardTrilha />
+    <div className="App">
+      <CardTrilha
+        nomeTrilha="Trilha da Costa da lagoa"
+        cidadeEstado="Florianópolis, SC"
+        duracao="3 horas"
+        trajeto="4 Km"
+        dificuldade="Iniciante"
+        tipoTrilha="Seletiva"
+        nomeCriador="Junior Pinho"
+        imagemURL="https://images.pexels.com/photos/2132087/pexels-photo-2132087.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
